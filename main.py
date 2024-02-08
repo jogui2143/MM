2120e7d077b197b52d58d6e1c8c2743963dea274
jogui2143 <jogui2143@hotmail.com>
2024-02-08 15:26:00 +0000

    :) :)

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ def user_colormap(img):
  colors += ['black'] if len(colors) < 2 else colors
  colormap(img, colors)
 
-user_colormap(nature)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+### Teste do colormap
+#user_colormap(nature)
 
 def isolate_rgb(img):
   return img[:,:,0], img[:,:,1], img[:,:,2]
@@ -59,7 +61,9 @@ def show_rgb_channels(img):
   blue = clr.LinearSegmentedColormap.from_list('cmap', ['black', 'blue'], N=256)
   show_imgs(['Red', 'Green', 'Blue'], (r, g, b), cm=[red, green, blue])
 
-show_rgb_channels(nature)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+### Teste do isolamento e união dos canais RGB
+#show_rgb_channels(nature)
 
 def padding(img):
   # Captura a altura (h) e a largura (ln) da imagem.
@@ -90,12 +94,14 @@ def padding(img):
 def unpadding(img, og):
   return img[:og[0], :og[1], :]
 
-padded_img, og = padding(nature)
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+### Teste do padding e unpadding
+#padded_img, og = padding(nature)
 
-unpadded_img = unpadding(padded_img, og)
+#unpadded_img = unpadding(padded_img, og)
 
-print(f'Dimensão Original: {nature.shape[:2]}')
-print(f'Dimensão Padded: {padded_img.shape[:2]}\nDimensão Unpadded: {unpadded_img.shape[:2]}')
+#print(f'Dimensão Original: {nature.shape[:2]}')
+#print(f'Dimensão Padded: {padded_img.shape[:2]}\nDimensão Unpadded: {unpadded_img.shape[:2]}')
 
 
 def RGB_to_YCbCr(img):
@@ -103,15 +109,58 @@ def RGB_to_YCbCr(img):
                 [-0.168736, -0.331264, 0.5],
                 [0.5, -0.418688, -0.081312]])
 
+  r = img[:,:,0]
+  g = img[:,:,1]
+  b = img[:,:,2]
 
-def encoder(img, isolate_rgb = False):
+  y = cm[0,0]*r + cm[0,1]*g + cm[0,2]*b
+  cb = cm[1,0]*r + cm[1,1]*g + cm[1,2]*b + 128
+  cr = cm[2,0]*r + cm[2,1]*g + cm[2,2]*b + 128
+
+  return np.dstack((y, cb, cr))
+
+def YCbCr_to_RGB(img):
+  convmatrix = np.array([[0.299, 0.587, 0.114],
+                [-0.168736, -0.331264, 0.5],
+                [0.5, -0.418688, -0.081312]])
+  cm_inv = np.linalg.inv(convmatrix)
+
+  y = img[:,:,0]
+  cb = img[:,:,1] - 128
+  cr = img[:,:,2] - 128
+
+  r = cm_inv[0,0]*y + cm_inv[0,1]*cb + cm_inv[0,2]*cr
+  g = cm_inv[1,0]*y + cm_inv[1,1]*cb + cm_inv[1,2]*cr
+  b = cm_inv[2,0]*y + cm_inv[2,1]*cb + cm_inv[2,2]*cr
+
+  output_matrix = np.dstack((r, g, b))
+  output_matrix = np.round(output_matrix)
+  output_matrix[output_matrix > 255] = 255
+  output_matrix[output_matrix < 0] = 0
+  return output_matrix
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+### Teste do RGB para YCbCr e YCbCr para RGB
+#nature_YcbCr = RGB_to_YCbCr(nature)
+#nature_RGB = YCbCr_to_RGB(nature_YcbCr)
+#print(f'Pixel 0x0 original: {nature_RGB[0, 0, :]}')
+#print(f'Pixel 0x0 dps: {nature[0, 0, :]}')
+#print(f'Pixel 0x0 equivalente: {nature[0, 0, :] == nature_RGB[0, 0, :]}')
+
+def encoder(img, isolate_rgb = False, padding = False, rgb_to_ycbcr = False):
   if isolate_rgb:
     return isolate_rgb(img)
+  elif padding:
+    return padding(img)
+  elif rgb_to_ycbcr:
+    return RGB_to_YCbCr(img)
 
-def decoder(img, unite_rgb = False):
+def decoder(img, unite_rgb = False, unpadding = False, ycbcr_to_rgb = False):
   if unite_rgb:
     return unite_rgb(img)
-
-#show image
+  elif unpadding:
+    return unpadding(img)
+  elif ycbcr_to_rgb:
+    return YCbCr_to_RGB(img)
 
 
